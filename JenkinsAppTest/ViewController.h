3c1954def1587552b08633c62e21439b972b0577
Arcanfel <archican91@gmail.com>
2013-09-14 21:31:37 -0400

    Initial commit

---
+++ JenkinsAppTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JenkinsAppTest
+//
+//  Created by Artur Shinkevich on 9/14/2013.
+//  Copyright (c) 2013 Artur Shinkevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ JenkinsAppTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JenkinsAppTest
+//
+//  Created by Artur Shinkevich on 9/14/2013.
+//  Copyright (c) 2013 Artur Shinkevich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
